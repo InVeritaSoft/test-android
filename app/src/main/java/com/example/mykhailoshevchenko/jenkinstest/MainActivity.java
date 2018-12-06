d35699a5e0fccbae95d040a5b3e0ff8a3d935c9b
Mykhailo Shevchenko <mikleshef@gmail.com>
2018-12-06 10:42:34 +0200

    Update Viktor Shnyrka

--- app/src/main/java/com/example/mykhailoshevchenko/jenkinstest/MainActivity.java
+++ app/src/main/java/com/example/mykhailoshevchenko/jenkinstest/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         //test
         //test
 //test
+//TEST
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
